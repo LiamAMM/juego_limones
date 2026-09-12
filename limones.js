e835dc4a1a34507f48d93c864d35944f71d3b409
LiamAMM <arielmorales142578@gmail.com>
2026-09-12 17:50:30 -0500

    Alerta GameOver al tener 0 Vidas

--- limones.js
+++ limones.js
@@ -83,8 +83,11 @@ function detectarPiso(){
         aparecerLimon();
         vidas=vidas-1;
         mostrarEnSpan("txtVidas",vidas);
-
+        if(vidas==0){
+            alert("GAME OVER");
+        }
     }
+    
 }
 
 
